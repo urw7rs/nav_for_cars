f4f8e91af14704954957b802baed5f848133339f
urw7rs <urw.7rs@icloud.com>
2022-07-29 13:47:03 +0900

    fix python package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["path_tracking"],
+    packages=["nav_for_cars"],
     package_dir={"": "src"},
 )
 
